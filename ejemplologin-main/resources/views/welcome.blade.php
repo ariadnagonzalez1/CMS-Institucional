d7b2f9d48838b5c4a01a0215360ff101e1ae66da
Ariadna Gonzalez <gonzalezariadna168@gmail.com>
2026-03-17 20:16:45 -0300

    cambios

--- ejemplologin-main/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('titulo', 'Listado de Clientes')
-
-@section('contenido')
-    <h1 class="text-2xl font-bold mb-4">Bienvenido al Crud de Clientes</h1>
-    
-    @livewire('clientes')
-@endsection
